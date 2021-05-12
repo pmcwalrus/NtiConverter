98f12a192c988e6dc2f222fafba7ad53ea107051
pmcwalrus <pmcwalrus@gmail.com>
2021-05-12 12:07:09 +0300

    Реализована запись сигналов в устройствах modbus

--- XmlFunctions.cs
+++ XmlFunctions.cs
@@ -1,4 +1,5 @@
 ﻿using Nti.XlsxReader.Types;
+using System;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -67,11 +68,35 @@ namespace NtiConverter
                 sb.AppendLine($"\t\t<connection host=\"{device.Network2}\" " +
                     $"port=\"502\" " +
                     $"parm=\"{device.DeviceName}_con2\"/>");
+
+                var signals = GetDeviceSignals(data, device.DeviceName);
+                sb.AppendLine(signals);
+
                 var addition = data.DeviceAdds.FirstOrDefault(x => x.DeviceName == device.DeviceName);
                 if (addition != null)
                     sb.AppendLine(addition.Addition);
                 sb.AppendLine("\t</device>");
                 sb.Append("\r\n");
+            }
+            return sb.ToString();
+        }
+
+        public static string GetDeviceSignals(NtiBase data, string deviceName)
+        {
+            var sb = new StringBuilder();
+            var signals = data.Layout.Where(x => x.DeviceIndex == deviceName).ToList();
+            foreach(var signal in signals)
+            {
+                var param = data.Signals.FirstOrDefault(x => x.Index == signal.SignalName);
+                if (param == null) continue;
+                string modbusType;
+                if (signal.Type.Contains("DI")) modbusType = "0x02";
+                else if (signal.Type.Contains("DO")) modbusType = "0x0F";
+                else if (signal.Type.Contains("AI")) modbusType = "0x04";
+                else if (signal.Type.Contains("AO")) modbusType = "0x03";
+                else modbusType = "???";
+                sb.AppendLine($"\t\t<address start_addr=\"{signal.NumberStr}\" " +
+                    $"type=\"{modbusType}\" parm=\"{param.SystemId}_{param.SignalId}\"/>");
             }
             return sb.ToString();
         }
